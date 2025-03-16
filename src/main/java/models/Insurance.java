05e2ff54ecbe87de094e4220538c2d94734b34cd
la-edvin777 <la.edvin1998@gmail.com>
2025-03-16 14:39:06 +0000

    More fixes. Insurance table is now displayed

--- src/main/java/models/Insurance.java
+++ src/main/java/models/Insurance.java
@@ -5,14 +5,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Insurance extends BaseModel<Insurance> {
-    private String insuranceID;
+    private String insuranceid;
     private String company;
     private String address;
     private String phone;
 
     // Constructor
-    public Insurance(String insuranceID, String company, String address, String phone) {
-        this.insuranceID = insuranceID;
+    public Insurance(String insuranceid, String company, String address, String phone) {
+        this.insuranceid = insuranceid;
         this.company = company;
         this.address = address;
         this.phone = phone;
@@ -22,8 +22,8 @@ public class Insurance extends BaseModel<Insurance> {
     public Insurance() {}
 
     // Getters and Setters
-    public String getInsuranceID() { return insuranceID; }
-    public void setInsuranceID(String insuranceID) { this.insuranceID = insuranceID; }
+    public String getInsuranceid() { return insuranceid; }
+    public void setInsuranceid(String insuranceid) { this.insuranceid = insuranceid; }
     public String getCompany() { return company; }
     public void setCompany(String company) { this.company = company; }
     public String getAddress() { return address; }
@@ -33,13 +33,13 @@ public class Insurance extends BaseModel<Insurance> {
 
     @Override
     protected String getTableName() {
-        return "Insurance";
+        return "insurance";
     }
 
     @Override
     protected Insurance mapResultSetToEntity(ResultSet rs) throws SQLException {
         return new Insurance(
-            rs.getString("insuranceID"),
+            rs.getString("insuranceid"),
             rs.getString("company"),
             rs.getString("address"),
             rs.getString("phone")
@@ -48,7 +48,7 @@ public class Insurance extends BaseModel<Insurance> {
 
     @Override
     protected void setCreateStatement(PreparedStatement stmt, Insurance entity) throws SQLException {
-        stmt.setString(1, entity.getInsuranceID());
+        stmt.setString(1, entity.getInsuranceid());
         stmt.setString(2, entity.getCompany());
         stmt.setString(3, entity.getAddress());
         stmt.setString(4, entity.getPhone());
@@ -59,47 +59,47 @@ public class Insurance extends BaseModel<Insurance> {
         stmt.setString(1, entity.getCompany());
         stmt.setString(2, entity.getAddress());
         stmt.setString(3, entity.getPhone());
-        stmt.setString(4, entity.getInsuranceID());
+        stmt.setString(4, entity.getInsuranceid());
     }
 
     @Override
     protected String getCreateSQL() {
-        return "INSERT INTO Insurance (insuranceID, company, address, phone) VALUES (?, ?, ?, ?)";
+        return "INSERT INTO insurance (insuranceid, company, address, phone) VALUES (?, ?, ?, ?)";
     }
 
     @Override
     protected String getUpdateSQL() {
-        return "UPDATE Insurance SET company = ?, address = ?, phone = ? WHERE insuranceID = ?";
+        return "UPDATE insurance SET company=?, address=?, phone=? WHERE insuranceid=?";
     }
 
     @Override
     protected String getDeleteSQL() {
-        return "DELETE FROM Insurance WHERE insuranceID = ?";
+        return "DELETE FROM insurance WHERE insuranceid=?";
     }
 
     @Override
     protected String getSelectAllSQL() {
-        return "SELECT * FROM Insurance";
+        return "SELECT * FROM insurance";
     }
 
     @Override
     protected String getSelectByIdSQL() {
-        return "SELECT * FROM Insurance WHERE insuranceID = ?";
+        return "SELECT * FROM insurance WHERE insuranceid=?";
     }
 
     @Override
     protected void setIdParameter(PreparedStatement stmt, Object id) throws SQLException {
-        stmt.setString(1, (String) id);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Insurance[ID=%s, Company=%s, Phone=%s]", 
-            insuranceID, company, phone);
+        stmt.setString(1, (String)id);
     }
 
     @Override
     protected Object getId() {
-        return insuranceID;
+        return insuranceid;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Insurance{insuranceid='%s', company='%s', address='%s', phone='%s'}", 
+            insuranceid, company, address, phone);
     }
 } 
